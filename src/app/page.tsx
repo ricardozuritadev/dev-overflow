98e31771cba024874bdbacf4e67e1c7045ade3d9
Ricardo Zurita <ricardo.zurita.dev@gmail.com>
2025-07-31 17:05:20 -0500

    Setup fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
     return (
         <div>
-            <h1>Hello World</h1>
+            <h1 className="h1-bold">Este es Inter</h1>
+            <h1 className="h1-bold font-space-grotesk">
+                Este es Space Grotesk
+            </h1>
         </div>
-    )
+    );
 }
