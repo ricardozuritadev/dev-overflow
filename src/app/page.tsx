902f95ff707309c287d15b588b5ac0af43bcd03e
Ricardo Zurita <ricardo.zurita.dev@gmail.com>
2025-07-31 17:54:23 -0500

    Modify layout and page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
 export default function Home() {
     return (
         <div>
-            <h1 className="h1-bold">Este es Inter</h1>
-            <h1 className="h1-bold font-space-grotesk">
-                Este es Space Grotesk
-            </h1>
+            <h1 className="font-space-grotesk h1-bold">DevOverflow</h1>
         </div>
     );
 }
